ac7dba463ce7dbbb1616bd500f916db469b1bfda
Luke Lombardi <luke@beam.cloud>
2026-02-04 11:02:13 -0500

    remove login to airstore item in tray

--- pkg/tray/ui_managed.go
+++ pkg/tray/ui_managed.go
@@ -2,31 +2,13 @@
 
 package tray
 
-import (
-	"os/exec"
-	"runtime"
+// Managed build - no additional menu items needed.
+// Users should run `airstore login` from terminal to authenticate.
 
-	"fyne.io/systray"
-	"github.com/beam-cloud/airstore/pkg/cli"
-)
-
-var mLogin *systray.MenuItem
-
-func addManagedMenuItems(u *ui) {
-	mLogin = systray.AddMenuItem("Login to Airstore...", "")
-}
+func addManagedMenuItems(u *ui) {}
 
 func getManagedClickCh() <-chan struct{} {
-	if mLogin != nil {
-		return mLogin.ClickedCh
-	}
 	return make(chan struct{})
 }
 
-func handleManagedClick() {
-	cmd := "xdg-open"
-	if runtime.GOOS == "darwin" {
-		cmd = "open"
-	}
-	exec.Command(cmd, cli.DashboardURL()+"/login").Start()
-}
+func handleManagedClick() {}
