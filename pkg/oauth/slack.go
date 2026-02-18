5b6d85eeccf10a14a062394347ef42f8e3b72b94
Cooper <cooper@beam.cloud>
2026-02-17 18:59:38 -0800

    Revert "Better channel handling"
    This reverts commit 0ed96f4107f58756c168626a0c303273de92d921.

--- pkg/oauth/slack.go
+++ pkg/oauth/slack.go
@@ -20,8 +20,6 @@ var slackIntegrationScopes = map[string][]string{
 	"slack": {
 		"channels:read",
 		"channels:history",
-		"groups:read",
-		"groups:history",
 		"files:read",
 		"search:read",
 		"users:read",
