9804e256882264e62a7ddf1b89870aecd9c14e35
Cooper <cooper@beam.cloud>
2026-03-02 19:28:15 -0800

    Oauth

--- pkg/gateway/gateway.go
+++ pkg/gateway/gateway.go
@@ -135,6 +135,7 @@ func NewGateway() (*Gateway, error) {
 	oauthRegistry.Register(oauth.NewNotionProvider(config.OAuth.Notion, callbackURL))
 	oauthRegistry.Register(oauth.NewSlackProvider(config.OAuth.Slack, callbackURL))
 	oauthRegistry.Register(oauth.NewLinearProvider(config.OAuth.Linear, callbackURL))
+	oauthRegistry.Register(oauth.NewAtlassianProvider(config.OAuth.Atlassian, callbackURL))
 
 	// Initialize S2 client for task log streaming if configured
 	var s2Client *common.S2Client
@@ -813,6 +814,7 @@ func (g *Gateway) initSources() {
 	g.sourceRegistry.Register(providers.NewLinearProvider())
 	g.sourceRegistry.Register(providers.NewPostHogProvider())
 	g.sourceRegistry.Register(providers.NewWebProvider(g.Config.Sources.Firecrawl.APIKey))
+	g.sourceRegistry.Register(providers.NewConfluenceProvider())
 
 	log.Info().Strs("providers", g.sourceRegistry.List()).Msg("source providers registered")
 }
